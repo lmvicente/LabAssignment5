b1f1a41c752e2b2bf90dba817514c7acec2c49f4
lmvicente <leah.m.vicente@gmail.com>
2018-11-08 09:42:01 -0800

    update

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+interface IStudent {
+  id: number;
+  firstName: string;
+  lastName: string;
+  course: string;
+}
+
 @Component({
   selector: 'app-main-content',
   templateUrl: './main-content.component.html',
@@ -7,9 +14,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainContentComponent implements OnInit {
 
-  constructor() { }
+
+  students: Array<IStudent> = [];
+
+  constructor() {
+    this.students[0] = {
+      id: 1,
+      firstName: 'Leah',
+      lastName: 'Vicente',
+      course: 'Business'
+    };
+
+    this.students[1] = {
+      id: 2,
+      firstName: 'John',
+      lastName: 'Smith',
+      course: 'Programming'
+    };
+
+    this.students[2] = {
+      id: 3,
+      firstName: 'Jane',
+      lastName: 'Doe',
+      course: 'Math'
+    };
+
+    this.students[3] = {
+      id: 4,
+      firstName: 'Joe',
+      lastName: 'Biden',
+      course: 'Politics'
+    };
+  }
 
   ngOnInit() {
   }
 
+  addStudent() {
+  const student: IStudent = {
+    id: 5,
+    firstName: 'Mike',
+    lastName: 'Tyson',
+    course: 'Boxing'
+  };
+
+    this.students.push(student);
+  }
 }
--- src/app/main-header/main-header.component.ts
+++ src/app/main-header/main-header.component.ts
@@ -1,13 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+
   selector: 'app-main-header',
   templateUrl: './main-header.component.html',
   styleUrls: ['./main-header.component.css']
 })
 export class MainHeaderComponent implements OnInit {
 
-  constructor() { }
+  private firstName: string;
+  private lastName: string;
+
+   constructor() {
+     this.firstName = 'Leah';
+     this.lastName = 'Vicente';
+   }
+
+   showGreeting() {
+       const banner = document.getElementById('banner');
+       banner.innerHTML = 'Hello' + ' ' + this.firstName + ' ' + this.lastName + '.';
+   }
+
+   hideGreeting() {
+       const banner = document.getElementById('banner');
+       banner.innerHTML = 'r1c2';
+   }
 
   ngOnInit() {
   }
